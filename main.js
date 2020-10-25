e0332a1de0db3ed5a0c5e725a4db7cb303878d7a
Urameshi801 <70042912+Urameshi801@users.noreply.github.com>
2020-10-24 19:10:33 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var citiesList =[];
+var citiesName;
+
+initCityList();
+initWeather(
+
+function renderCities(){
+    $("#citiesList").empty();
+    $("#cityinput").val("");
+
+    for (i=0 i<citiesList.length; i++){
+        var a = $("<a>");
+        a. addClass("list-Group-item")
+    }
+
+}
